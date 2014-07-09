17313f593feda2648c82ee34b3fd39561972dd5c
Chris Ortner <chris@codexfons.com>
2014-07-09 09:27:31 +0200

    project outline

---
+++ src/com/codexfons/compositesplit/CommandLineInterface.java
@@ -0,0 +1,13 @@
+package com.codexfons.compositesplit;
+
+public class CommandLineInterface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
